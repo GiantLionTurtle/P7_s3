aa19cbb68bd8335d96278e894fd981475ce22dc3
GiantLionTurtle <charle.tanguay@gmail.com>
2024-07-04 15:22:31 -0400

    regler bug slot et signal

--- rpi/GUI/build/moc_mainwindow.cpp
+++ rpi/GUI/build/moc_mainwindow.cpp
@@ -23,7 +23,7 @@ QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
     QByteArrayData data[9];
-    char stringdata0[91];
+    char stringdata0[92];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,13 +39,13 @@ QT_MOC_LITERAL(3, 30, 11), // "sendCommand"
 QT_MOC_LITERAL(4, 42, 19), // "std::vector<double>"
 QT_MOC_LITERAL(5, 62, 6), // "accels"
 QT_MOC_LITERAL(6, 69, 9), // "sendState"
-QT_MOC_LITERAL(7, 79, 5), // "State"
-QT_MOC_LITERAL(8, 85, 5) // "state"
+QT_MOC_LITERAL(7, 79, 5), // "state"
+QT_MOC_LITERAL(8, 85, 6) // "setPID"
 
     },
     "MainWindow\0receiveFromSerial\0\0sendCommand\0"
     "std::vector<double>\0accels\0sendState\0"
-    "State\0state"
+    "state\0setPID"
 };
 #undef QT_MOC_LITERAL
 
@@ -55,7 +55,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,14 +63,16 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x08 /* Private */,
-       3,    1,   32,    2, 0x08 /* Private */,
-       6,    1,   35,    2, 0x08 /* Private */,
+       1,    1,   34,    2, 0x08 /* Private */,
+       3,    1,   37,    2, 0x08 /* Private */,
+       6,    1,   40,    2, 0x08 /* Private */,
+       8,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QString,    2,
     QMetaType::Void, 0x80000000 | 4,    5,
-    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -83,7 +85,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->receiveFromSerial((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 1: _t->sendCommand((*reinterpret_cast< std::vector<double>(*)>(_a[1]))); break;
-        case 2: _t->sendState((*reinterpret_cast< State(*)>(_a[1]))); break;
+        case 2: _t->sendState((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->setPID(); break;
         default: ;
         }
     }
@@ -118,13 +121,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
