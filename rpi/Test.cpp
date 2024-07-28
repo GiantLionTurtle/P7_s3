aacd76eba40f5b48b78e8168b0375877ff46daa0
Maxime <maxime.lebl@outlook.com>
2024-07-28 11:41:26 -0400

    Fix Simu2

--- rpi/Test.cpp
+++ rpi/Test.cpp
@@ -1,18 +1,18 @@
-#include "Simulation_V2.hpp"
+#include "Simulation_V3.hpp"
+#include <iostream>
 
 
 int main()
 {
-    Simulation_V2 sim(0.04);
 
-    std::vector<double> Seq = sim.RunSimulation(0, 1.373468, 100, 0, 0);
-
+    std::vector<double> Seq = runSimulation(0, 1.373468, 100, 0, 0);
+    std::cout << "Premiere Simu:\n";
     for(auto s: Seq)
     {
         std::cout << s << "\n";
     }
 
-    Seq = sim.RunSimulation(0, 0.1, 10, 0, 0);
+    Seq = runSimulation(0, 0.1, 10, 0, 0);
     std::cout << "Deuxieme Simu:\n";
     for(auto s: Seq)
     {
